769234d4eaf5861f5017be30e30c7c971dfe2f9a
gorin.k <gorin.k@nio3.niip.ru>
2019-09-13 14:56:53 +0300

    another

--- files/.sh/amount.sh
+++ files/.sh/amount.sh
@@ -1,7 +1,7 @@
 . $sh/dmenurc.sh
 
 # NAME,FSTYPE,LABEL,FSUSE%,FSSIZE,MOUNTPOINT 
-target=$( sudo lsblk -o NAME,FSTYPE,LABEL,SIZE,MOUNTPOINT | grep sd | dmenu $DMENU_OPTIONS | cut -f 1 -d " " | tr -cd '[:alnum:]' )
+target=$( sudo lsblk -o NAME,FSTYPE,LABEL,SIZE,MOUNTPOINT | grep .sd | dmenu $DMENU_OPTIONS | cut -f 1 -d " " | tr -cd '[:alnum:]' )
 echo $target
 if [ ! -z "${target}" ] ; then
     udevil mount /dev/$target &> /tmp/vil1 
