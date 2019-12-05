9b695c27ab47ca325f67df7e217364c221ddad27
gorin.k <gorin.k@nio3.niip.ru>
2019-12-05 18:28:47 +0300

    another

--- files/.sh/listzathura.sh
+++ files/.sh/listzathura.sh
@@ -1,5 +1,5 @@
 source $sh/dmenurc.sh
-targ=$(find -L /ln/hh /ln/ho/use /ln/dwn $HOME/.cache/luakit/viewpdf/ -type f \( -iname "*.pdf" -or -iname "*.djvu" \) | dmenu $DMENU_OPTIONS)
+targ=$( (find -L /ln/ho -maxdepth 1 -type f \( -iname "*.pdf" -or -iname "*.djvu" \) && find -L /ln/hh /ln/ho/use /ln/dwn $HOME/.cache/luakit/viewpdf/ -type f \( -iname "*.pdf" -or -iname "*.djvu" \)) | dmenu $DMENU_OPTIONS)
 
 if [ -n "$targ" ] ; then
     zathura "$targ"
