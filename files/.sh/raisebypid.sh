f0828e1c625e3215b51806772d03cfc758a2e78b
plasmik <mail@is.com>
2020-01-09 22:33:50 +0300

    another

---
+++ files/.sh/raisebypid.sh
@@ -0,0 +1,13 @@
+
+mypid=$1
+idlist=$(xprop -root _NET_CLIENT_LIST)
+idlist=${st##*#}
+idlist=${st//,}
+
+for i in $idlist ; do
+  PID=$(xprop -id $i _NET_WM_PID)
+  PID=${PID##*= }
+  echo $PID
+  [[ "$PID" == "$mypid" ]] && xdotool windowactivate $i && break
+done 
+
