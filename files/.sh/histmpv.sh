7c943f74b6f7cd9ac7891092bd73260ddc7a86a5
plasmik <mail@is.com>
2020-03-02 15:16:49 +0300

    another

--- files/.sh/histmpv.sh
+++ files/.sh/histmpv.sh
@@ -3,5 +3,6 @@ d=$(tac /tmp/mpv-history | dmenu $DMENU_OPTIONS -fn "$DMENU_FN")
 if [ -z "$d" ]; then echo no selection !; exit; fi
 cd "$d"
 echo $d
-xterm -xrm 'XTerm.vt100.allowTitleOps: false' -T "$d -- mpv" -geometry 60x24+400+250 -e 'mpv-album --no-video .' &> /dev/null &
+# xterm -xrm 'XTerm.vt100.allowTitleOps: false' -T "$d -- mpv" -geometry 60x24+400+250 -e 'mpv-album --no-video .' &> /dev/null &
+$TERMINAL -e /bin/bash -c "mpv-album ." 
 
--- files/.sh/trmflushred.sh
+++ files/.sh/trmflushred.sh
@@ -1,7 +1,7 @@
 eval `ssh-agent`
 # ssh-add ~/.ssh/ss.k &
 
-rsync -e ssh -av --progress --no-perms --no-times --delete --size-only /ln/torrents/red/ pi@192.168.1.78:/ln/red/
+rsync -e ssh -avK --progress --stats --no-perms --no-times --delete --size-only /ln/torrents/red/ pi@192.168.1.78:/ln/red/
 
 cd /ln/torrents/red/.torrents-new/
 for f in *.torrent
