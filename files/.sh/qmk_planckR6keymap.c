10e2fc37db9f1a143c2ec8d38919495ad3763c39
6vod <gorin.k@nio3.niip.ru>
2020-06-09 10:44:03 +0300

    another

--- files/.sh/qmk_planckR6keymap.c
+++ files/.sh/qmk_planckR6keymap.c
@@ -51,21 +51,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
+ * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  Up  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |  \   |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * |  \   |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Down |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | GUI  |  [   |  ]   |Lower |Space |  '   |Raise | Left | Rght | Alt  |Shift |
+ * | Ctrl | GUI  | Alt  |Numer |Lower |Space |  '   |Raise |   [  |   ]  | Del  |Shift |
  * `-----------------------------------------------------------------------------------'
 */
 
 [_QWERTY] = LAYOUT_planck_grid(
-    SFT_T(KC_ESC)  , KC_Q   , KC_W    , KC_E  , KC_R  , KC_T   , KC_Y   , KC_U   , KC_I    , KC_O    , KC_P    , KC_BSPC ,
-    KC_TAB  , KC_A   , KC_S    , KC_D  , KC_F  , KC_G   , KC_H   , KC_J   , KC_K    , KC_L    , KC_SCLN , KC_UP   ,
-    KC_DEL  , KC_Z   , KC_X    , KC_C  , KC_V  , KC_B   , KC_N   , KC_M   , KC_COMM , KC_DOT  , KC_SLSH , KC_DOWN ,
-    KC_LCTL , KC_LGUI , KC_LALT , NUMER , LOWER , KC_SPC , NU_T(KC_QUOT) , RAISE , KC_LBRC , KC_RBRC , KC_BSLS , SFT_T(KC_ENT)
+    SFT_T(KC_ESC) , KC_Q    , KC_W    , KC_E  , KC_R  , KC_T   , KC_Y          , KC_U  , KC_I    , KC_O    , KC_P    , KC_BSPC       ,
+    KC_TAB        , KC_A    , KC_S    , KC_D  , KC_F  , KC_G   , KC_H          , KC_J  , KC_K    , KC_L    , KC_SCLN , KC_UP         ,
+    KC_BSLS       , KC_Z    , KC_X    , KC_C  , KC_V  , KC_B   , KC_N          , KC_M  , KC_COMM , KC_DOT  , KC_SLSH , KC_DOWN       ,
+    KC_LCTL       , KC_LGUI , KC_LALT , NUMER , LOWER , KC_SPC , NU_T(KC_QUOT) , RAISE , KC_LBRC , KC_RBRC , KC_DEL  , SFT_T(KC_ENT)
 ),
 
 // [_QWERTY] = LAYOUT_planck_grid(
