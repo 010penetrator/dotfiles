54c1297f8ded9adebb08c6e8d5635c9c9a6f1257
plasmik <mail@is.com>
2020-07-12 17:55:35 +0300

    another

--- files/.sh/muslistupd.sh
+++ files/.sh/muslistupd.sh
@@ -1,7 +1,7 @@
 . $sh/dmenurc.sh
 find -L /ln/mus/ -type f \( -name "*.flac" -or -name "*.mp3" -or -name "*.ogg" -or -name "*.wv" -or -name "*.ape" -or -name "*.wma" \) -printf '%h\n' | sort -u > .mus-list
 a=$(wc -l .mus-list | cut -d " " -f1)
-find -L /ln/____/ -type f \( -name "*.flac" -or -name "*.mp3" -or -name "*.ogg" -or -name "*.wv" -or -name "*.ape" -or -name "*.wma" \) -printf '%h\n' | sort -u >> .mus-list
+find -L /ln/__/ -type f \( -name "*.flac" -or -name "*.mp3" -or -name "*.ogg" -or -name "*.wv" -or -name "*.ape" -or -name "*.wma" \) -printf '%h\n' | sort -u >> .mus-list
 find -L /ln/torrents/ -type f \( -name "*.flac" -or -name "*.mp3" -or -name "*.ogg" -or -name "*.wv" -or -name "*.ape" -or -name "*.wma" \) -printf '%h\n' | sort -u >> .mus-list
 b=$(wc -l .mus-list | cut -d " " -f1)
 # (echo .:. MUSIC FAMILIARITY .:. $a of $b $'\n'; cat .mus-list) | dmenu $DMENU_OPTIONS -fn "$DMENU_FN"
