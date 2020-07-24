eba3467e4dda410f2fa10eb4c259364d2a36ebd8
6vod <gorin.k@nio3.niip.ru>
2020-07-24 13:32:15 +0300

    another

--- files/.sh/ctags_with_deps.sh
+++ files/.sh/ctags_with_deps.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+TMPFILE=/tmp/ctagsourcelist
+touch $TMPFILE
 
-gcc -M $* | sed -e 's/[\\ ]/\n/g' | \
+gcc -M $* 2>/dev/null | sed -e 's/[\\ ]/\n/g' | \
     sed -e '/^$/d' -e '/\.o:[ \t]*$/d' | \
+    tee $TMPFILE | sort -u | \
     ctags -L - --c++-kinds=+p --fields=+iaS --extra=+q
+[ $? -eq 0 ] && echo Ctags done! || echo Ctags ERROR!
+
 
