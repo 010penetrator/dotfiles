e7013930929c9d28fc822cebf971c22ece730ec7
6vod <gorin.k@nio3.niip.ru>
2020-08-11 11:12:26 +0300

    another

--- files/.sh/qmk_Atreis_keymap.c
+++ files/.sh/qmk_Atreis_keymap.c
@@ -8,19 +8,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // 0: Base Layer
 KEYMAP(
-    SFT_T(KC_ESC) , KC_1     , KC_2   , KC_3    , KC_4    , KC_5      , KC_6    , KC_7    , KC_8    , KC_9   , KC_0    , KC_BSPC   ,
-    SFT_T(KC_TAB) , KC_Q     , KC_W   , KC_E    , KC_R    , KC_T      , KC_Y    , KC_U    , KC_I    , KC_O   , KC_P    , KC_UP     ,
-    KC_LALT       , KC_A     , KC_S   , KC_D    , KC_F    , KC_G      , KC_H    , KC_J    , KC_K    , KC_L   , KC_SCLN , KC_DOWN   ,
-    KC_LCTL       , KC_Z     , KC_X   , KC_C    , KC_V    , KC_B      , KC_N    , KC_M    , KC_COMM , KC_DOT , KC_SLSH , F_T(KC_DEL) ,
-            MO(1) , KC_SPC  ,  KC_LGUI , KC_QUOT , KC_ENT  , KC_LSFT ,  KC_LBRC ,  CTL_T(KC_RBRC)  ),
+SFT_T(KC_ESC) , KC_1     , KC_2   , KC_3    , KC_4    , KC_5      , KC_6    , KC_7    , KC_8    , KC_9   , KC_0    , KC_BSPC   ,
+SFT_T(KC_TAB) , KC_Q     , KC_W   , KC_E    , KC_R    , KC_T      , KC_Y    , KC_U    , KC_I    , KC_O   , KC_P    , KC_UP     ,
+KC_LALT       , KC_A     , KC_S   , KC_D    , KC_F    , KC_G      , KC_H    , KC_J    , KC_K    , KC_L   , KC_SCLN , KC_DOWN   ,
+KC_LCTL       , KC_Z     , KC_X   , KC_C    , KC_V    , KC_B      , KC_N    , KC_M    , KC_COMM , KC_DOT , KC_SLSH , F_T(KC_DEL) ,
+        MO(1) , KC_SPC  ,  KC_LGUI , KC_QUOT , KC_ENT  , KC_LSFT ,  KC_LBRC ,  CTL_T(KC_RBRC)  ),
 
-  // 1: Function Layer
+// 1: Function Layer
 KEYMAP(
-    KC_ESC        ,  KC_F1    ,  KC_F2   ,  KC_F3   ,  KC_F4   ,  KC_F5   ,  KC_F6   ,  KC_F7    ,  KC_F8   ,  KC_F9    ,  KC_F10 ,  KC_DEL   ,
-    KC_TAB        ,  KC_Q     ,  KC_PGUP ,  KC_END  ,  KC_R    ,  KC_TILD ,  KC_Y    ,  KC_U     ,  KC_INS  ,  KC_F11   ,  KC_F12 ,  KC_PGUP  ,
-    KC_LALT       ,  KC_HOME  ,  KC_LSFT ,  KC_MINS ,  KC_PGDN ,  KC_GRV  ,  KC_LEFT ,  KC_DOWN  ,  KC_UP   ,  KC_RIGHT ,  KC_QUOT,  KC_PGDN  ,
-    KC_LCTL       ,  KC_UNDS  ,  KC_MINS ,  KC_PLUS ,  KC_EQL  ,  KC_PGUP ,  KC_BSLS ,  KC_MINS  ,  KC_UNDS ,  KC_PLUS  ,   KC_EQL , KC_DEL   ,
-            MO(1) ,  KC_SPC  ,   KC_LGUI ,  KC_QUOT ,  KC_CAPSLOCK , KC_RSFT , KC_LEFT , KC_RIGHT )
+KC_ESC        ,  KC_F1    ,  KC_F2   ,  KC_F3   ,  KC_F4   ,  KC_F5   ,  KC_F6   ,  KC_F7    ,  KC_F8   ,  KC_F9    ,  KC_F10 ,  KC_DEL   ,
+KC_TAB        ,  KC_Q     ,  KC_PGUP ,  KC_END  ,  KC_R    ,  KC_TILD ,  KC_Y    ,  KC_U     ,  KC_INS  ,  KC_F11   ,  KC_F12 ,  KC_PGUP  ,
+KC_LALT       ,  KC_HOME  ,  KC_LSFT ,  KC_MINS ,  KC_PGDN ,  KC_GRV  ,  KC_LEFT ,  KC_DOWN  ,  KC_UP   ,  KC_RIGHT ,  KC_QUOT,  KC_PGDN  ,
+KC_LCTL       ,  KC_UNDS  ,  KC_MINS ,  KC_PLUS ,  KC_EQL  ,  KC_PGUP ,  KC_BSLS ,  KC_MINS  ,  KC_UNDS ,  KC_PLUS  ,   KC_EQL , KC_DEL   ,
+        MO(1) ,  KC_CAPSLOCK , KC_LGUI , KC_QUOT ,  KC_BSLS  , KC_RSFT , KC_LEFT , KC_RIGHT )
 };
 
 // Custom Actions
