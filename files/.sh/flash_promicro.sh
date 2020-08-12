0bf27692d17892de0c7f45b02f35c2004af0560b
6vod <gorin.k@nio3.niip.ru>
2020-08-12 08:56:49 +0300

    another

--- files/.sh/flash_promicro.sh
+++ files/.sh/flash_promicro.sh
@@ -10,12 +10,12 @@ ls /dev/tty* > /tmp/1
 echo "Reset your Pro Micro now"
 
 while [[ -z $USB ]]; do
-  sleep 1
+  sleep .3
   ls /dev/tty* > /tmp/2
   USB=`diff /tmp/1 /tmp/2 | grep -o '/dev/tty.*'`
 done
 
-sleep 1
+sleep .4
 
 # stty -F $USB speed 1200 
 # stty -F $USB speed 9600 
@@ -23,3 +23,4 @@ sleep 1
 
 avrdude -p atmega32u4 -c avr109 -P $USB -U flash:w:$TARGET
 # avrdude -p m32U4 -c avr109 -P $USB -U flash:w:$TARGET
+
