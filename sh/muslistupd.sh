8afcf595bb669d55708a97c008e05eeeeefd0742
archuser <cool@mail.no>
2020-10-02 07:36:46 +0300

    another

--- sh/muslistupd.sh
+++ sh/muslistupd.sh
@@ -3,6 +3,7 @@ find -L /ln/mus/ -type f \( -name "*.flac" -or -name "*.mp3" -or -name "*.ogg" -
 a=$(wc -l .mus-list | cut -d " " -f1)
 find -L /ln/__/ -type f \( -name "*.flac" -or -name "*.mp3" -or -name "*.ogg" -or -name "*.wv" -or -name "*.ape" -or -name "*.wma" \) -printf '%h\n' | sort -u >> .mus-list
 find -L /ln/torrents/ -type f \( -name "*.flac" -or -name "*.mp3" -or -name "*.ogg" -or -name "*.wv" -or -name "*.ape" -or -name "*.wma" \) -printf '%h\n' | sort -u >> .mus-list
+find -L /ln/dwn/ -type f \( -name "*.flac" -or -name "*.mp3" -or -name "*.ogg" -or -name "*.wv" -or -name "*.ape" -or -name "*.wma" \) -printf '%h\n' | sort -u >> .mus-list
 b=$(wc -l .mus-list | cut -d " " -f1)
 # (echo .:. MUSIC FAMILIARITY .:. $a of $b $'\n'; cat .mus-list) | dmenu $DMENU_OPTIONS -fn "$DMENU_FN"
 echo .:. MUSIC FAMILIARITY .:. $a of $b
---
+++ sh/respic.sh
@@ -0,0 +1,3 @@
+# Make half-size decent jpg with imagemagick
+mkdir respic ; for f in *.*; do convert "$f" -resize $1%  -sampling-factor 4:2:0  -quality 95%  jpg:respic/"$f" ; done
+
