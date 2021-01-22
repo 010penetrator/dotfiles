6407d2618edbb890ec36ee3bc421b4b3bc987adc
gorin.k <gorin.k@nio3.niip.ru>
2021-01-22 09:56:58 +0300

    another

--- sh/rc.sh
+++ sh/rc.sh
@@ -5,8 +5,8 @@ cd
 # height=$(xdpyinfo | grep dimensions | cut -d ' ' -f 7 | cut -d x -f 2)
 # [[ height -gt 1100 ]] && export HIDPI=1 || export HIDPI=0
 
-desk_len=$(xdpyinfo | grep dimensions | cut -d ' ' -f 7 | cut -d x -f 1)
-[[ desk_len -gt 1900 ]] && export HIDPI=1 || export HIDPI=0
+desk_len=$(xdpyinfo | grep dimensions | cut -d ' ' -f 7 | cut -d x -f 2)
+[[ desk_len -gt 1200 ]] && export HIDPI=1 || export HIDPI=0
 
 source ~/.bashrc
 
