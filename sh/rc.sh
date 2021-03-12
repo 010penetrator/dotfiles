ededb54d60c0e509574aee681b5e120f49a907d8
gorin.k <gorin.k@nio3.niip.ru>
2021-03-12 18:06:05 +0300

    another

--- sh/rc.sh
+++ sh/rc.sh
@@ -74,6 +74,8 @@ fi
 # sleep .9
 # notify-send $HOSTNAME &
 
+killall osdsh ; osdsh -c blue -p 0 -a 2 -d 1 -f -adobe-helvetica-bold-r-*-*-*-240-*-*-*-*-iso8859-*
+
 # echo RC done &
 notify-send 'RC complete' &
 
