79b197a1f295d6a4951d5868d18acd1c4c67c299
gorin.k <gorin.k@nio3.niip.ru>
2021-04-26 20:07:50 +0300

    another

--- sh/respic.sh
+++ sh/respic.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # Resize all images in dir to half and compress to jpg with imagemagick
 
-mkdir respic ; for f in *.*; do convert "$f" -resize $1%  -sampling-factor 4:2:0  -quality 95%  jpg:respic/"$f".jpg ; done
+[ -z "$1" ] && set -- "50%"
+
+mkdir -p respic
+for f in *.png ; do
+    convert "$f" -resize "$1" -sampling-factor 4:2:0  -quality 95%  jpg:respic/"$f".jpg;
+done
+for f in *.jpg ; do
+    convert "$f" -resize "$1" -sampling-factor 4:2:0  -quality 95%  jpg:respic/"$f";
+done
 
