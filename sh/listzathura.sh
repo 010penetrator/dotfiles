53a586a77a06daa600b386d0bff3cabc2e2e3b5f
gorin.k <gorin.k@nio3.niip.ru>
2021-06-04 13:53:00 +0300

    another

--- sh/listzathura.sh
+++ sh/listzathura.sh
@@ -2,7 +2,7 @@
 # Choose from pdf and djvu files with dmenu. And open it with zathura
 
 source $sh/dmenurc.sh
-targ=$( (find -L /ln/ho -maxdepth 1 -type f \( -iname "*.pdf" -or -iname "*.djvu" \) && find -L /ln/hh /ln/ho/use /ln/dwn $HOME/.cache/luakit/viewpdf/ -type f \( -iname "*.pdf" -or -iname "*.djvu" \)) | dmenu $DMENU_OPTIONS -fn "$DMENU_FN")
+targ=$( (find -L /ln/ho -maxdepth 1 -type f \( -iname "*.pdf" -or -iname "*.djvu" \) && find -L /ln/hh /ln/ho/aa /ln/ho/use /ln/dwn $HOME/.cache/luakit/viewpdf/ -type f \( -iname "*.pdf" -or -iname "*.djvu" \)) | dmenu $DMENU_OPTIONS -fn "$DMENU_FN")
 
 if [ -n "$targ" ] ; then
     zathura "$targ"
