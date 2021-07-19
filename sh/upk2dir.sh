5c8a48051be110f67728840ecd15e98fcc4f7373
plasmik <plasmik@nomail>
2021-07-19 14:13:41 +0300

    another

--- sh/upk2dir.sh
+++ sh/upk2dir.sh
@@ -1,11 +1,14 @@
+#!/bin/bash
+echo upk2dir:
 echo args are $*
 ground=${PWD}
 dirname=${PWD##*/}
-echo ground is $ground 
-if [ -z "$1" ] ; then 
-  echo Specify target 
-  exit 
+# echo ground is $ground 
+if [ -z "$1" ] || [ -n "$3" ] || [ -d "$1" ] || [ -f "$2" ]; then 
+  echo Usage: upk2dir.sh archive.file [OUTDIR]
+  exit 1
 fi
+
 if [[ "$2" ]] ; then 
   mkdir -p "$2/$1" || { echo Destination unavailable.; exit 1; } 
   dest="$2/$1"
@@ -18,7 +21,7 @@ fi
 
 # mv "$1" "$dest"
 cd "$dest"
-upk.sh "$ground/$1" || exit 1
+upk.sh "$ground/$1" || { echo Unpacking encountered problems.; exit 1; }
 
 if [ "$dirname" != ".del" ] ; then
   cd "$ground" && [ -d ".del" ] && mv "$1" .del/"$1"
