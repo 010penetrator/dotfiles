2f13a1be3249d5ad304c1babebf9f5731bf29815
plasmik <plasmik@nomail>
2021-08-03 07:52:13 +0300

    another

--- sh/trmfetchdef.sh
+++ sh/trmfetchdef.sh
@@ -1,3 +1,3 @@
-for f in /ln/dwn/*.torrent ; do echo . ; echo "$f" ; transmission-remote 9090 -a "$f" && sleep 0.5 && mv "$f" ~/.del ; done
+for f in /ln/dwn/*.torrent ; do echo . ; echo "$f" ; transmission-remote 9090 -a "$f" && sleep 0.5 && mv "$f" ~/.del/ ; done
 sleep 1 
 
