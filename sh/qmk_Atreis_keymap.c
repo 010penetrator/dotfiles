4c94006e263461b453d074bf435085303bbe10a6
plasmik <plasmik@nomail>
2021-08-12 23:47:20 +0300

    another

--- sh/qmk_Atreis_keymap.c
+++ sh/qmk_Atreis_keymap.c
@@ -12,7 +12,7 @@ SFT_T(KC_ESC) , KC_1     , KC_2   , KC_3    , KC_4    , KC_5     , KC_6    , KC_
 SFT_T(KC_TAB) , KC_Q     , KC_W   , KC_E    , KC_R    , KC_T     , KC_Y    , KC_U    , KC_I    , KC_O   , KC_P    , KC_UP     ,
 KC_LALT       , KC_A     , KC_S   , KC_D    , KC_F    , KC_G     , KC_H    , KC_J    , KC_K    , KC_L   , KC_SCLN , KC_DOWN   ,
 KC_LCTL       , KC_Z     , KC_X   , KC_C    , KC_V    , KC_B     , KC_N    , KC_M    , KC_COMM , KC_DOT , KC_SLSH , F_T(KC_DEL) ,
-        MO(1) , KC_SPC   , KC_LGUI,  KC_QUOT , KC_ENT , KC_LSFT  , KC_LBRC ,  CTL_T(KC_RBRC)  ),
+        MO(1) , KC_SPC   , KC_LGUI,  KC_ENT , KC_QUOT , KC_LSFT  , KC_LBRC ,  CTL_T(KC_RBRC)  ),
 
 // 1: Function Layer
 KEYMAP(
@@ -20,7 +20,7 @@ KC_GRV        , KC_F1    ,  KC_F2   ,  KC_F3   ,  KC_F4   ,  KC_F5   ,  KC_F6   
 KC_TILD       , KC_Q     ,  KC_PGUP ,  KC_END  ,  KC_R    ,  KC_TILD ,  KC_Y    ,  KC_U     ,  KC_INS  ,  KC_F11   ,  KC_F12  ,  KC_PGUP  ,
 KC_LALT       , KC_HOME  ,  KC_S    ,  KC_DEL  ,  KC_PGDN ,  KC_GRV  ,  KC_LEFT ,  KC_DOWN  ,  KC_UP   ,  KC_RIGHT ,  KC_QUOT ,  KC_PGDN  ,
 KC_LCTL       , KC_MINS  ,  KC_PLUS ,  KC_UNDS ,  KC_EQL  ,  KC_PGUP ,  KC_BSLS ,  KC_MINS  ,  KC_PLUS ,  KC_UNDS  ,  KC_EQL  ,  KC_DEL   ,
-        MO(1) , KC_CAPSLOCK , KC_LGUI , KC_QUOT , KC_BSLS ,  KC_RSFT ,  KC_LEFT ,  KC_RIGHT )
+        MO(1) , KC_CAPSLOCK , KC_LGUI , KC_BSLS , KC_BSLS ,  KC_RSFT ,  KC_LEFT ,  KC_RIGHT )
 };
 
 // Custom Actions
