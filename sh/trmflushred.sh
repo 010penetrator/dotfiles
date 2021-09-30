19e1f8173e9eea98327a6b061b451311f0eddb28
plasmik <plasmik@nomail>
2021-09-30 08:25:32 +0300

    another

--- sh/trmflushred.sh
+++ sh/trmflushred.sh
@@ -1,16 +1,18 @@
-eval `ssh-agent`
+# eval `ssh-agent`
 # ssh-add ~/.ssh/ss.k &
 
 # rsync -e ssh -avK --bwlimit=300K --progress --stats --no-perms --no-times --delete --size-only /ln/torrents/red/ admin@192.168.1.160:/ln/red/
 rsync -e ssh -avK --progress --stats --no-perms --no-times --delete --size-only /ln/torrents/red/ pi@192.168.1.78:/ln/red/
 
-cd /ln/torrents/red/.torrents-new/
+# cd /ln/torrents/red/.torrents-new/
+cd /ln/ho/.rtorrent/watch/red/
 for f in *.torrent
 do
   if [ -f "$f" ] ; then
     echo $f
-    mkdir -p del
-    transmission-remote 192.168.1.78:9090 -a -w /ln/red/ "$f" && mv "$f" del/
+    # mkdir -p del
+    transmission-remote 192.168.1.78:9090 -a -w /ln/red/ "$f"
+    # && mv "$f" del/
   fi
 done
 cd -
