e945312fa10a2bb9873746e72cd8d515e70f8427
plasmik <plasmik@nomail>
2022-01-26 00:25:06 +0300

    another

--- sh/raise.sh
+++ sh/raise.sh
@@ -12,38 +12,40 @@ activeid=$(echo $activeid | cut -d ',' -f1)
 while [ ${#activeid} -lt 7 ] ; do activeid=0$activeid; done
 echo activeid is $activeid
 if [ "$1" == '0' ]
+# if argument is null, cycle through windows of same class as current
 then 
-	wname=$(wmctrl -l -x | grep $activeid | cut -d " " -f4 | cut -d "." -f1)
-	echo focus another $wname
+  wname=$(wmctrl -l -x | grep $activeid | cut -d " " -f4 | cut -d "." -f1)
+  echo focus another $wname
 else
-	wname=$1
-	if [ -z "$1" ] ; then wname="543yhv89nt7u8h43q34q8gh" ; fi # to select no candidate
+  wname=$1
+  if [ -z "$1" ] ; then echo "Please, provide argument"; exit 1 ; fi
 fi
 
-#If window of that name is active, there are others of that name, current is not last in list
+# If window of that name is active, there are others of that name, current is not last in list
 if
-	[[ $(wmctrl -l -x | grep $wname | grep $activeid) ]] && 
-	[[ $(wmctrl -l -x | grep -v -e "^...........-" | grep $wname | wc -l) > 1 ]] &&
-	# [[ $(wmctrl -l -x | grep $wname | wc -l) != $(wmctrl -l -x | grep $wname | grep -n $activeid | cut -d ":" -f1) ]]
-	[[ $(wmctrl -l -x | grep $wname | grep -A1 $activeid | wc -l) > 1 ]]
+  [[ $(wmctrl -l -x | grep "$wname" | grep $activeid) ]] &&
+  [[ $(wmctrl -l -x | grep -v -e "^...........-" | grep "$wname" | wc -l) > 1 ]] &&
+  # [[ $(wmctrl -l -x | grep $wname | wc -l) != $(wmctrl -l -x | grep $wname | grep -n $activeid | cut -d ":" -f1) ]]
+  [[ $(wmctrl -l -x | grep "$wname" | grep -A1 $activeid | wc -l) > 1 ]]
 then
-	#Need to choose next window of same name
-	echo "choosing next"
-	str=$(wmctrl -l -x | grep -v -e "^...........-" | grep $wname | grep -A1 $activeid | tail -1 | cut -c -10)
+  # Need to choose next window of same name
+  echo "choosing next"
+  str=$(wmctrl -l -x | grep -v -e "^...........-" | grep "$wname" | grep -A1 $activeid | tail -1 | cut -c -10)
 else
-	str=$(wmctrl -l -x | grep -v -e "^...........-" | grep -m 1 $wname | cut -c -10)
+  echo s1
+  str=$(wmctrl -l -x | grep -v -e "^...........-" | grep -m 1 "$wname" | cut -c -10)
 fi
 
 echo Final aim $str
 if [ -z "$str" ]
-	then
-		if [ -n "$2" ]
-			#Exec
-			then
+  then
+    if [ -n "$2" ]
+      #Exec
+      then
         eval " source $HOME/.bashrc ; $2 " &> /dev/null &
-			else
+      else
         $wname &> /dev/null &
-		fi
-	else wmctrl -i -a $str
+    fi
+  else wmctrl -i -a $str
 fi
 
