484c9c2474d87321fb1d6add273fe4e45570f9b0
plasmik <plasmik@nomail>
2022-02-25 19:03:47 +0300

    another

--- sh/sync.sh
+++ sh/sync.sh
@@ -6,28 +6,46 @@
 
 dirin=$1
 dirout=$2
+RSYNC_OPTS="-rt --progress --delete --size-only"
 
-read -p "Do dry run first? (y/n)" prompt
-[[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]] &&
+echo -e "\n=============================================\nStarting sync script"
+read -rs -n1 -p $'\nhit Anykey to dry-run, "S" or Esc to skip it\n' prompt
+# [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]] &&
 
-for dir in "$dirout"/*/ ; do
-  dir=${dir%*/}
-  dir=${dir##*/}
-  #echo "$dirin"/"$dir"/ ; read noth
-  if [ -d "$dirin"/"$dir"/ ] ; then echo "$dirin"/"$dir"/ ;  # sleep 3;
-    rsync -rtn --delete --progress "$dirin"/"$dir"/ "$dirout"/"$dir"
-  fi
-done
+if ! [[ $prompt == "s" || $prompt == "S" || $prompt == $'\e' ]] ; then
+  echo -e "\n========== dry-run"
+  for dir in "$dirout"/*/ ; do
+    dir=${dir%*/}
+    dir=${dir##*/}
+    final_in="$dirin/$dir"
+    final_out="$dirout/$dir"
+    if [ -d "$final_in"/ ] ; then
+      echo "  >>" "$final_in" ">>" "$final_out" ;  # sleep 3;
+      rsync -n $RSYNC_OPTS "$final_in"/ "$final_out"
+    fi
+  done
+  echo -e "========== dry-run done"
+fi
 
-read -p "Press ENTER to sync or Ctrl-C to exit" prompt
+read -rs -n1 -p $'\nhit Enter or Space to sync, Anykey to exit\n' prompt
 
-for dir in "$dirout"/*/ ; do
-  dir=${dir%*/}
-  dir=${dir##*/}
-  #echo "$dirin"/"$dir"/ ; read noth
-  if [ -d "$dirin"/"$dir"/ ] ; then
-    rsync -rt --delete --progress "$dirin"/"$dir"/ "$dirout"/"$dir"
-  fi
-done
+if [[ $prompt == "" || $prompt == " " ]] ; then
+  echo -e "\n========== rsync"
+  for dir in "$dirout"/*/ ; do
+    dir=${dir%*/}
+    dir=${dir##*/}
+    final_in="$dirin/$dir"
+    final_out="$dirout/$dir"
+    #echo "$dirin"/"$dir"/ ; read noth
+    if [ -d "$final_in"/ ] ; then
+      echo "  >>" "$final_in" ">>" "$final_out" ;  # sleep 3;
+      rsync -rt $RSYNC_OPTS "$final_in"/ "$final_out"
+    fi
+  done
+  echo -e "========== rsync done"
+else
+  echo "got $prompt, will exit."
+fi
 
+sleep 1
 
