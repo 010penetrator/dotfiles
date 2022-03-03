0b7a9c82557a6fa54492777c0111a8dcb2cd3539
plasmik <plasmik@nomail>
2022-03-03 19:05:45 +0300

    another

--- sh/select-kitty-theme.sh
+++ sh/select-kitty-theme.sh
@@ -1,35 +1,49 @@
 #!/bin/bash
 # Sorry, no documentation for this
 
-go_through () {
+go_through ()
+{
+  unset key
   echo
   echo will go through $PWD
-  echo key commands are : f d q a
-  echo Themes are:
-  ls -x *.conf
-  echo
-  for t in *.conf
-  do
-    echo $t
-    kitty @ set-colors -a "$PWD/$t"
-    read -rsn 1 key
-    [[ $key == 'f' ]] && mkdir -p favs && cp "$t" "favs/$t"
-    [[ $key == 'd' ]] && mkdir -p del && mv "$t" "del/$t"
-    [[ $key == 'q' ]] && break
-    [[ $key == '' ]] && echo CR
-    [[ $key == 'a' ]] && cp -f $t ~/.config/kitty/theme.conf && exit
-  done 
-  echo
-  if [ -d favs ] && [ "$( ls -A favs/ )" ] ; then
-    echo --Entering next level of favourites
-    cd favs
-  else
-    echo --Gonna crawl this list again
+  if  ! [[ $( ls -A *.conf 2>/dev/null ) ]] ; then
+    echo Theme files not found here!
+    exit
   fi
-  read -rsn 1 -p $'Press "q" to exit, anykey to go through themes\n' key
-  [[ $key == 'q' ]] && return 0
-  go_through
+  while (true) ; do 
+    if [[ $key == 'i' ]] ; then
+      if [[ -d favs ]] && [[ $( ls -A favs/*.conf 2>/dev/null ) ]] ; then
+        cd favs
+        echo --Entering next level of favourites
+      else
+        echo --Favs not found! Gonna crawl this list again
+      fi
+    fi
+    #
+    echo ------------
+    echo Themes are:
+    ls -x *.conf
+    echo ------------
+    echo current theme is $(readlink /ln/co/kitty/theme.conf)
+    echo
+    #
+    for t in *.conf
+    do
+      unset key
+      echo $t
+      kitty @ set-colors -a "$PWD/$t"
+      read -rsn 1 key
+      [[ $key == 'f' ]] && mkdir -p favs && cp "$t" "favs/$t"
+      [[ $key == 'd' ]] && mkdir -p del && mv "$t" "del/$t"
+      [[ $key == 'q' ]] && exit
+      [[ $key == 'i' ]] && break
+      [[ $key == 'b' ]] && break
+      [[ $key == 'a' ]] && ln -srf "$t" ~/.config/kitty/theme.conf
+    done 
+    echo
+  done
 }
 
+echo key commands are : f d q a i b 
 cd ~/.config/kitty-themes/themes/ && go_through
 
