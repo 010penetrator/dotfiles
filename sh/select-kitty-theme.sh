5cb23d7158187f323b2fdbef5bac9e9a05962e24
plasmik <plasmik@nomail>
2022-03-08 14:46:50 +0300

    another

--- sh/select-kitty-theme.sh
+++ sh/select-kitty-theme.sh
@@ -38,7 +38,7 @@ go_through ()
       [[ $key == 'q' ]] && exit
       [[ $key == 'i' ]] && break
       [[ $key == 'b' ]] && break
-      [[ $key == 'a' ]] && ln -srf "$t" ~/.config/kitty/theme.conf && echo --Applied!
+      [[ $key == 'a' ]] && ln -srf "$t" ~/.config/kitty/theme.conf && echo --Applied! && exit
     done 
     echo
   done
