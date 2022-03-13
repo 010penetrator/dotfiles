db448146516982824128d4dc99c8f31e660a3ad9
plasmik <plasmik@nomail>
2022-03-13 20:17:33 +0300

    another

--- sh/conf/qmk_Atreis_keymap.c
+++ sh/conf/qmk_Atreis_keymap.c
@@ -8,11 +8,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // 0: Base Layer
 KEYMAP(
-SFT_T(KC_ESC) , KC_1     , KC_2   , KC_3    , KC_4    , KC_5     , KC_6    , KC_7    , KC_8    , KC_9   , KC_0    , KC_BSPC   ,
-SFT_T(KC_TAB) , KC_Q     , KC_W   , KC_E    , KC_R    , KC_T     , KC_Y    , KC_U    , KC_I    , KC_O   , KC_P    , KC_UP     ,
-KC_LALT       , KC_A     , KC_S   , KC_D    , KC_F    , KC_G     , KC_H    , KC_J    , KC_K    , KC_L   , KC_SCLN , KC_DOWN   ,
-KC_LCTL       , KC_Z     , KC_X   , KC_C    , KC_V    , KC_B     , KC_N    , KC_M    , KC_COMM , KC_DOT , KC_SLSH , F_T(KC_ENT) ,
-        MO(1) , KC_SPC   , KC_LGUI,  KC_DEL , KC_QUOT , KC_LSFT  , KC_LBRC ,  RCTL_T(KC_RBRC)  ),
+SFT_T(KC_ESC) , KC_1     , KC_2     , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9   , KC_0    , KC_BSPC   ,
+SFT_T(KC_TAB) , KC_Q     , KC_W     , KC_E    , KC_R    , KC_T    , KC_Y    , KC_U    , KC_I    , KC_O   , KC_P    , KC_UP     ,
+KC_LALT       , KC_A     , KC_S     , KC_D    , KC_F    , KC_G    , KC_H    , KC_J    , KC_K    , KC_L   , KC_SCLN , KC_DOWN   ,
+KC_LCTL       , KC_Z     , KC_X     , KC_C    , KC_V    , KC_B    , KC_N    , KC_M    , KC_COMM , KC_DOT , KC_SLSH , F_T(KC_ENT) ,
+          MO(1) , KC_SPC , KC_LGUI ,  KC_DEL ,                      KC_QUOT , KC_LSFT , KC_LBRC , RCTL_T(KC_RBRC) ),
 
 // 1: Function Layer
 KEYMAP(
@@ -20,7 +20,7 @@ KC_GRV        , KC_F1    , KC_F2   ,  KC_F3   , KC_F4   , KC_F5   , KC_F6   , KC
 KC_TILD       , KC_Q     , KC_PGUP ,  KC_END  , KC_R    , KC_TILD , KC_Y    , KC_U     , KC_INS  , KC_F11   , KC_F12  , KC_PGUP ,
 KC_LALT       , KC_HOME  , KC_S    ,  KC_D    , KC_PGDN , KC_GRV  , KC_LEFT , KC_DOWN  , KC_UP   , KC_RIGHT , KC_QUOT , KC_PGDN ,
 KC_LCTL       , KC_MINS  , KC_PLUS ,  KC_UNDS , KC_EQL  , KC_PGUP , KC_BSLS , KC_MINS  , KC_PLUS , KC_UNDS  , KC_EQL  , KC_ENT  ,
-      MO(1) , KC_CAPSLOCK , KC_LGUI , KC_BSLS , KC_BSLS , KC_RSFT , KC_LEFT , KC_RIGHT )
+     MO(1) , LSFT(KC_SPC) , KC_LGUI , KC_CAPSLOCK ,                 LSFT(KC_BSLS) , KC_RSFT , KC_LEFT , KC_RIGHT )
 };
 
 // Custom Actions
@@ -29,10 +29,10 @@ const uint16_t PROGMEM fn_actions[] = {
 };
 
 // const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-// 	// keyevent_t event = record->event;
-// 	switch (id) {
-// 	}
-// 	return MACRO_NONE;
+//     // keyevent_t event = record->event;
+//     switch (id) {
+//     }
+//     return MACRO_NONE;
 // }
 
 void matrix_init_user(void) {
@@ -42,25 +42,25 @@ void matrix_scan_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	return true;
+    return true;
 }
 
 // void led_set_user(uint8_t usb_led) {
-// 	if (usb_led & (1 << USB_LED_NUM_LOCK)) {
-// 	} else {
-// 	}
-// 	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-// 	} else {
-// 	}
-// 	if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
-// 	} else {
-// 	}
-// 	if (usb_led & (1 << USB_LED_COMPOSE)) {
-// 	} else {
-// 	}
-// 	if (usb_led & (1 << USB_LED_KANA)) {
-// 	} else {
-// 	}
+//     if (usb_led & (1 << USB_LED_NUM_LOCK)) {
+//     } else {
+//     }
+//     if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
+//     } else {
+//     }
+//     if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
+//     } else {
+//     }
+//     if (usb_led & (1 << USB_LED_COMPOSE)) {
+//     } else {
+//     }
+//     if (usb_led & (1 << USB_LED_KANA)) {
+//     } else {
+//     }
 // }
 
 
