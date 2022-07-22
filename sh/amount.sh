f48da5daf04aad51058f79ac5b9a76ce7a1d004c
plasmik <plasmik@nomail>
2022-07-23 02:41:17 +0300

    another

--- sh/amount.sh
+++ sh/amount.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 # Mount block device with udevil and dmenu.
 
-source $sh/dmenurc
-SELECTOR="dmenuy"
+[[ -z $SELECTOR ]] && SELECTOR="dmenu" && source $sh/dmenurc && SELECTOR="dmenuy"
 
 LSBLK="lsblk"
 # NAME,FSTYPE,LABEL,FSUSE%,FSSIZE,MOUNTPOINT
-target=$( $LSBLK -o NAME,FSTYPE,LABEL,SIZE,MOUNTPOINT | grep -e ".sd\|.nvme" |
-     $SELECTOR |
-     cut -f 1 -d " " | tr -cd '[:alnum:]' )
-if [ ! -z "${target}" ]; then
-    udevil mount /dev/$target 2>&1 | tee /tmp/vil1
+target=$( $LSBLK -o TYPE,NAME,FSTYPE,LABEL,SIZE,MOUNTPOINT -l | grep "part" | cut -f 2- -d " " | 
+     $SELECTOR | cut -f 1 -d " " )
+if [[ ! -z target ]]; then
+    udevil mount /dev/$target 2>&1 | tee /tmp/vil-mo
     if [ ${PIPESTATUS[0]} -eq "0" ]; then
         echo "/dev/$target mounted"
         mopoint=$(findmnt /dev/${target} -o TARGET -n)
-        [[ -d mopoint ]] && touch -c "$mopoint" && return 0
+        [[ -d mopoint ]] && touch -c "$mopoint"
     fi
-    notify-send "$(cat /tmp/vil1)"
+    notify-send "$(cat /tmp/vil-mo)"
 else
     echo "No target!"
 fi
+
--- sh/aumount.sh
+++ sh/aumount.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 # Umount block device with udevil and dmenu.
 
-source $sh/dmenurc
+[[ -z $SELECTOR ]] && SELECTOR="dmenu" && source $sh/dmenurc && SELECTOR="dmenuy"
 
 LSBLK="lsblk"
-target=$( $LSBLK -o NAME,FSTYPE,LABEL,SIZE,MOUNTPOINT | grep -e ".sd\|.nvme" | grep '/' | dmenuy | cut -f 1 -d " " | tr -cd '[:alnum:]' )
-# mopoint=$( mount | grep "${target}" | cut -f 3 -d ' ' ); [ ! -z "${target}" ]
-[ ! -z "${target}" ] && sync "/dev/$target" && ( udevil umount /dev/$target &> /tmp/vil2 && notify-send "/dev/$target unmounted" || notify-send "$(cat /tmp/vil2)" )
+target=$( $LSBLK -o NAME,FSTYPE,LABEL,SIZE,MOUNTPOINT -l | grep '/' |
+    $SELECTOR | cut -f 1 -d " " )
+if [[ ! -z target ]]; then
+    mopoint=$(findmnt /dev/${target} -o TARGET -n)
+    # sync "/dev/$target"
+    sync -f "$mopoint"
+    if [ $? -eq "0" ]; then
+        udevil umount /dev/$target 2>&1 | tee /tmp/vil-umo
+        if [ ${PIPESTATUS[0]} -eq "0" ]; then
+            echo "/dev/$target unmounted from $mopoint"
+            notify-send "/dev/$target unmounted from $mopoint"
+            # notify-send "$(cat /tmp/vil-umo)"
+        fi
+    else
+        notify-send "$(cat /tmp/vil-umo)"
+        notify-send "Not able to sync /dev/$target"
+    fi
+fi
 
