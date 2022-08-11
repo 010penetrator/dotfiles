c779e461b36141b2810f9bfdfe05eb6e09511d28
Guy <voaygrrr@here.lol>
2022-08-11 14:42:11 +0300

    another

--- sh/amount.sh
+++ sh/amount.sh
@@ -7,13 +7,13 @@ LSBLK="lsblk"
 target=$( $LSBLK -o TYPE,NAME,FSTYPE,LABEL,SIZE,MOUNTPOINT -l | grep "part" | cut -f 2- -d " " |
      $SELECTOR | cut -f 1 -d " " )
 
-if [[ ! -z $target ]]; then # target is null or ''
+if [[ ! -z $target ]]; then # target isnot null or ''
     udevil mount /dev/$target 2>&1 | tee /tmp/vil-mo
     if [ ${PIPESTATUS[0]} -eq "0" ]; then
         echo "/dev/$target mounted"
         sleep .1
         mopoint=$(findmnt /dev/${target} -o TARGET -n)
-        [[ -d mopoint ]] && touch "$mopoint"
+        [[ -d $mopoint ]] && touch "$mopoint"
     fi
     notify-send "$(cat /tmp/vil-mo)"
 else
