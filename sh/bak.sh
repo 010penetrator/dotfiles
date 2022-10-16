d0c2f2e59d4f8d46d648311457e7b516baa18da5
plasmik <plasmik@nomail>
2022-10-16 11:32:55 +0300

    another

--- sh/bak.sh
+++ sh/bak.sh
@@ -5,16 +5,19 @@ mkdir -p "$bakhot"
 ! [[ -d $bakhot ]] && echo Error! Hot backup dir not available! && exit
 
 DAY=$(date +%Y-%m-%d)
+WEEK=$(date +%Y_w%W)
 MON=$(date +%Y-%m)
-if [[ -d $bakhot/$DAY ]] ; then
+DEST_HOT="$bakhot/$WEEK"
+
+if [[ -d $DEST_HOT ]] ; then
     if [[ $DONTOVERWRITE == 1 ]] ; then
         echo -e "-- Backup not wanted!"
         exit
     else
-        rm -rf "$bakhot/$DAY"
+        rm -rf "$DEST_HOT"
     fi
 fi
-mkdir "$bakhot/$DAY"
+mkdir "$DEST_HOT"
 
 [[ -d $bakhot/ffbookmarks ]] &&
 {
@@ -29,25 +32,25 @@ echo --bak.stage1
 tar -czf /ln/lo/cur/linkdir.tar.gz -C / ln
 
 [[ -d $tt ]] && conditional_line="-C $tt/../ tt"
-    tar -chzf "$bakhot/$DAY/words.tar.gz" --ignore-failed-read \
+    tar -chzf "$DEST_HOT/words.tar.gz" --ignore-failed-read \
         -C /ln/ --exclude=lo/cur sh lo \
         -C /tmp $boomname \
         $conditional_line
 
 [[ -d /ln/torrents ]] &&
-    tar -czf "$bakhot/$DAY/torrents.tar.gz" --ignore-failed-read \
+    tar -czf "$DEST_HOT/torrents.tar.gz" --ignore-failed-read \
         -C $HOME .config/transmission-daemon .rtorrent .rtorrent.rc
 
 # [[ -d /ln/co/joke ]] && conditional_line="-C /ln/co/ nvim/plugged"
 [[ -d /ln/co/nvim ]] && [[ $space == "ho" ]] &&
-    tar -cf "$bakhot/$DAY/plugged.tar.zst" -I "zstd -10 -T0" --exclude='.git' \
+    tar -cf "$DEST_HOT/plugged.tar.zst" -I "zstd -10 -T0" --exclude='.git' \
         -C /ln/co/nvim plugged
 
 [[ -d /ln/wo ]] && [[ $space == "wo" ]] &&
     workrp=$(realpath /ln/wo) && workbn=$(basename $workrp) &&
-    tar -cf "$bakhot/$DAY/work.tar.zst" -I "zstd -10 -T0" -C $workrp/.. $workbn
+    tar -cf "$DEST_HOT/work.tar.zst" -I "zstd -10 -T0" -C $workrp/.. $workbn
 
-tar -cf "$bakhot/$DAY/conf.tar.zst" -I "zstd -10 -T0" --ignore-failed-read \
+tar -cf "$DEST_HOT/conf.tar.zst" -I "zstd -10 -T0" --ignore-failed-read \
     -C / etc/fstab etc/udevil/udevil.conf \
     -C $HOME .ssh .bash_history $(ls .git*tials 2>/dev/null) .cache/dmenu-recent .vim \
     -C /ln/co kitty kitty-themes deadbeef \
@@ -57,7 +60,7 @@ tar -cf "$bakhot/$DAY/conf.tar.zst" -I "zstd -10 -T0" --ignore-failed-read \
     {
         echo --bak.stage2
         mkdir -p "$bakcld/${space}_$MON"
-        cp -rf "$bakhot/$DAY/"*tar* "$bakcld/${space}_$MON"
+        cp -rf "$DEST_HOT/"*tar* "$bakcld/${space}_$MON"
     } ||
         { echo Warning! Cold backup dir failed! ;}
 
@@ -66,6 +69,6 @@ bakusbdir=$(compgen -G /ln/mo/*AMV/bak)
     {
         echo --Gonna write to SAM as well..
         mkdir -p "$bakusbdir/${space}_$MON"
-        cp -rf "$bakhot/$DAY/"*tar* "$bakusbdir/${space}_$MON"
+        cp -rf "$DEST_HOT/"*tar* "$bakusbdir/${space}_$MON"
     }
 
