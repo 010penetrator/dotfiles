e3b4f6f5bbd1117ecca784c69e8b1553d419c771
winny <post>
2022-11-19 12:27:41 +0300

    another

--- sh/sethome.bat
+++ sh/sethome.bat
@@ -8,9 +8,10 @@ SET HOMESHARE="X:\"
 SET HOME=%USERPROFILE%
 ::SET TEMP=%HOME%\Local Settings\Temp
 ::SET TMP="%TEMP%"
-SET PATH=%PATH%;C:\exe\nvim-win64\bin
+SET PATH=%PATH%;C:\exe\nvim-win64\bin;"c:\program files\cmake\bin"
 set git=c:\compy
 set sh=%GIT%\dotfiles\sh
+set loc=c:\cmd
 
 %HOMEDRIVE%
 cd %HOMEPATH%
