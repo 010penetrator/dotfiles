693782df8b6d7284872f360c09dcefead9ed2b22
plasmik <plasmik@mail.no>
2022-12-11 15:47:12 +0300

    another

--- sh/trmflushred.sh
+++ sh/trmflushred.sh
@@ -6,7 +6,7 @@
 [[ -d /ln/torrents/red ]] || exit
 
 # rsync -e ssh -avK --bwlimit=300K --progress --stats --no-perms --no-times --delete --size-only /ln/torrents/red/ admin@192.168.1.160:/ln/red/
-rsync -e ssh -avK --progress --stats --no-perms --no-times --delete --size-only /ln/torrents/red/ pi@192.168.1.78:/ln/red/
+rsync -e ssh -avK --progress --stats --no-perms --no-times --delete --size-only /ln/torrents/red/ pi@192.168.1.79:/ln/red/
 
 # cd /ln/torrents/red/.torrents-new/
 cd /ln/ho/.rtorrent/watch/red/ || exit
@@ -17,11 +17,11 @@ do
         # echo $(date -r "$f" "+%Y-%m-%d")
         echo $(date -r "$f" "+%Y-%m-%d") "  " $f
         echo "$f"
-        transmission-remote 192.168.1.78:9090 -a -w /ln/red/ "$f"
+        transmission-remote 192.168.1.79:9090 -a -w /ln/red/ "$f"
     fi
     # if [[ -f $f ]] ; then
     #     echo $f
-    #     transmission-remote 192.168.1.78:9090 -a -w /ln/red/ "$f"
+    #     transmission-remote 192.168.1.79:9090 -a -w /ln/red/ "$f"
     # fi
 done
 cd -
