d10b5cdfe4c1691c6093e3386c273b418de4def4
plasmik <plasmik@mail.no>
2022-12-13 02:13:22 +0300

    another

--- sh/trmflushred.sh
+++ sh/trmflushred.sh
@@ -1,14 +1,13 @@
 # eval `ssh-agent`
 # ssh-add ~/.ssh/ss.k &
 
-[[ -z $NDAYS ]] && NDAYS=60 # (re-) establish .torrents under that age
+[[ -z $NDAYS ]] && NDAYS=20 # (re-) establish .torrents under that age
 
 [[ -d /ln/torrents/red ]] || exit
 
 # rsync -e ssh -avK --bwlimit=300K --progress --stats --no-perms --no-times --delete --size-only /ln/torrents/red/ admin@192.168.1.160:/ln/red/
 rsync -e ssh -avK --progress --stats --no-perms --no-times --delete --size-only /ln/torrents/red/ pi@192.168.1.79:/ln/red/
 
-# cd /ln/torrents/red/.torrents-new/
 cd /ln/ho/.rtorrent/watch/red/ || exit
 for f in *.torrent
 do
@@ -19,10 +18,6 @@ do
         echo "$f"
         transmission-remote 192.168.1.79:9090 -a -w /ln/red/ "$f"
     fi
-    # if [[ -f $f ]] ; then
-    #     echo $f
-    #     transmission-remote 192.168.1.79:9090 -a -w /ln/red/ "$f"
-    # fi
 done
 cd -
 
