7fd8f6cebdfdb166235bab037c8d24081c7f8678
plasmik <plasmik@mail.no>
2023-01-09 09:56:54 +0300

    another

--- sh/listgames.sh
+++ sh/listgames.sh
@@ -12,11 +12,11 @@ if [ $targ ] ; then
     Noita)
       # notify-send 1
       # cd "/ln/ho/Games/gog/noita/drive_c/GOG Games/Noita/"
-      path=$(realpath "/ln/ho/Games/Noita")
+      path=$(realpath "/ln/ho/.wine/drive_c/GOG Games/Noita")
       export WINEPREFIX="$path"
       export WINEARCH=win32
       export WINEPATH="$path"
-      cd "$path/drive_c/GOG Games/Noita/"
+      cd "$path"
       wine noita
     ;;
     Quake3)
