d859b655eb6c491cc0fc1ea30b8583ddecdbc1c4
plasmik <plasmik@mail.no>
2023-02-24 15:57:18 +0300

    another

--- sh/fetchreda_rt.sh
+++ sh/fetchreda_rt.sh
@@ -5,3 +5,5 @@ list=$( ls /ln/dwn/*.torrent | dmenuy )
 echo $list
 [[ -n list ]] && mv "$list" /ln/ho/.rtorrent/watch/red/
 
+    # transmission-remote 9090 -a -w /ln/torrents/red "$f" && sleep 0.5 && mv "$f" /ln/torrents/red/.torrents-new/
+
--- sh/fetchreda_trd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for f in /ln/dwn/*.torrent ; do
-    echo .
-    echo "$f"
-    transmission-remote 9090 -a -w /ln/torrents/red "$f" && sleep 0.5 && mv "$f" /ln/torrents/red/.torrents-new/
-done
-
