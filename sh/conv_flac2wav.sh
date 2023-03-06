b01ae0c27b6bbca048d8fe96f538ef0a3b96ba1b
plasmik <plasmik@mail.no>
2023-03-06 09:53:35 +0300

    another

---
+++ sh/conv_flac2wav.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Convert flac tracks to mp3 VBR0
+
+# $1 is origin path
+# $2 is destination
+
+if [ -z "$1" ] ; then set -- "." "${2}" ; fi
+if [ -z "$2" ] ; then set -- "${1}" "." ; fi
+
+cd $1
+
+drn=${PWD##*/}
+mkdir -p "$2/$drn"
+for f in *.flac;
+do ffmpeg -i "$f" "$2/$drn/$f.wav"
+done
+
+cp *[C,c]over.* "$2/$drn/"
+cp [F,f]ront.* "$2/$drn/"
+cp [F,f]older.* "$2/$drn/"
+cp [B,b]ack.* "$2/$drn/"
+
