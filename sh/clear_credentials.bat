663a7c962920e184f3627b1b4c2462944d507b05
user <user@winux>
2023-05-02 00:35:40 +0300

    another

---
+++ sh/clear_credentials.bat
@@ -0,0 +1,8 @@
+@echo off
+cmdkey.exe /list > "%TEMP%\List.txt"
+findstr.exe Target "%TEMP%\List.txt" > "%TEMP%\tokensonly.txt"
+FOR /F "tokens=1,2 delims= " %%G IN (%TEMP%\tokensonly.txt) DO cmdkey.exe /delete:%%H
+del "%TEMP%\List.txt" /s /f /q
+del "%TEMP%\tokensonly.txt" /s /f /q
+echo All done
+pause
