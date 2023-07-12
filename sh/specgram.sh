2c59894d6757c1d778813e74d502f13eafe3055f
plasmik <plasmik@mail.no>
2023-07-12 23:02:32 +0300

    ---

--- sh/specgram.sh
+++ sh/specgram.sh
@@ -2,6 +2,6 @@
 # See cool spectrogram of an audiofile
 # Needs 'sox' & 'feh'
 
-sox "$1" -n remix 2 spectrogram -x 1200 -y 660 -o/tmp/spec.png -p2 -t "$1" ; feh -B black /tmp/spec.png &>/dev/null
+sox "$1" -n remix 2 spectrogram -x 900 -y 550 -o/tmp/spec.png -p2 -t "$1" ; feh -B black /tmp/spec.png &>/dev/null
 # rm /tmp/spec.png
 
