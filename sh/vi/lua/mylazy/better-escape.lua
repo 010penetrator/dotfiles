7a4ee08718f96b8281f26a3dab99bbd809869ba1
user <user@winux>
2023-07-28 18:41:17 +0300

    ---

--- sh/vi/lua/mylazy/better-escape.lua
+++ sh/vi/lua/mylazy/better-escape.lua
@@ -1,7 +1,7 @@
 return {
     "max397574/better-escape.nvim",
     opts = {
-        mapping = {"jk", "jj"},
+        mapping = {"jk", "jj", "kj"},
         timeout = 220,
         clear_empty_lines = true,
     }
