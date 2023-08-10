b5fe0b510073a0948a99f107e3d22ef4807fde16
plasmik <plasmik@mail.no>
2023-08-10 12:21:22 +0300

    ---

--- sh/sxd.sh
+++ sh/sxd.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Reload sxhkd silently
+# And invoke useful stuff
 
 killall sxhkd; sxhkd -c  /ln/sh/conf/sxhkdrc 1>/dev/null &
+
+addlayout.sh
+xinputupd.sh
+xset r rate 220 30 # set keyboard repeat rate
+
+notify-send "$( LC_ALL=ru_RU.UTF-8 date; uptime -p )"
 # notify-send TERMINAL "$TERMINAL" #debug
 
