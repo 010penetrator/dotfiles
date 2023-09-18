82c14e213e15b304ccafd38b68aae93953a5a6a6
user <user@winux>
2023-09-18 19:29:42 +0300

    ---

--- sh/vi/lua/helpy.lua
+++ sh/vi/lua/helpy.lua
@@ -126,12 +126,26 @@ function M.rsetup(p)
   end
 end
 
-M.tableMerge = function(tSrc,tDest)
+--[[ M.tableMerge = function(tSrc,tDest)
   for k,v in pairs(tDest) do
     tSrc[k] = v
   end
-end
+end ]]
 
+function M.tableMerge(t1, t2)
+    for k,v in pairs(t2) do
+        if type(v) == "table" then
+            if type(t1[k] or false) == "table" then
+                tableMerge(t1[k] or {}, t2[k] or {})
+            else
+                t1[k] = v
+            end
+        else
+            t1[k] = v
+        end
+    end
+    return t1
+end
 
 ---Author: cseickel
 ---The file system path separator for the current platform.
