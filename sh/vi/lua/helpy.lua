c17f662da31fbaeda9f0a37cce0e36eb3c11a47d
user <user@winux>
2023-09-18 21:29:49 +0300

    ---

--- sh/vi/lua/helpy.lua
+++ sh/vi/lua/helpy.lua
@@ -133,18 +133,18 @@ end
 end ]]
 
 function M.tableMerge(t1, t2)
-    for k,v in pairs(t2) do
-        if type(v) == "table" then
-            if type(t1[k] or false) == "table" then
-                tableMerge(t1[k] or {}, t2[k] or {})
-            else
-                t1[k] = v
-            end
-        else
-            t1[k] = v
-        end
+  for k,v in pairs(t2) do
+    if type(v) == "table" then
+      if type(t1[k] or false) == "table" then
+        tableMerge(t1[k] or {}, t2[k] or {})
+      else
+        t1[k] = v
+      end
+    else
+      t1[k] = v
     end
-    return t1
+  end
+  return t1
 end
 
 ---Author: cseickel
