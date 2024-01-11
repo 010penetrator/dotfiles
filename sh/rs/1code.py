d054bb0e0474dd8c3303ba0768bb904b7ffc2107
user <user@winux>
2024-01-11 19:16:27 +0300

    another

--- sh/rs/1code.py
+++ sh/rs/1code.py
@@ -48,3 +48,39 @@ s = '\n'.join( s.split('\n')[i] for i in [0,2,3] )
 
 netmask = get_ipv4_netmask_from_nic(if_name, ip_candi) or DEFAULT_NETMASK
 cidr = ipaddress.IPv4Network('0.0.0.0/' + netmask).prefixlen
+
+
+def printBA(ba,cr=16):
+    # print()
+    for i in range(0,len(ba),cr):
+        line = ba[i:i+cr]
+        strHex = line.hex()
+        strHexPret = ""
+        for i in range(len(strHex)//2):
+            if i>0 and i%4 == 0:
+                strHexPret += ' '
+            if i>0:
+                strHexPret += ' '
+            strHexPret += strHex[i*2:i*2+2]
+        strChar = ""
+        for i,byte in enumerate(line):
+            if i>0 and i%4 == 0:
+                strChar += ' '
+            ch = chr(byte) if byte>31 else "◊"
+            # ▯▨▽◊◌
+            strChar += ch
+        print(strHexPret + "    " + strChar)
+    print()
+
+def zeroPadBA(limit, ba=bytearray.fromhex("00")):
+    while len(ba) < limit:
+        ba.append(0x00)
+
+def hexlify_bytestring(bytestring, delim=""):
+    ''' Very simple way to hexlify a byte string using delimiters '''
+    retval = ""
+    for intval in bytestring:
+        retval += ('0123456789ABCDEF'[int(intval / 16)])
+        retval += ('0123456789ABCDEF'[int(intval % 16)])
+        retval += delim
+    return retval
