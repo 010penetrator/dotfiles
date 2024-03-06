5bfb2a493a493316b5dd1aad84c2a089ed9771c6
plasmik <plasmik@mail.no>
2024-03-06 23:45:18 +0300

    another

--- sh/muslibupd.sh
+++ sh/muslibupd.sh
@@ -6,14 +6,14 @@ rm $muslibrary
 PATH_OLD="/ln/mus"
 PATH_NEW="/ln/mus1/ /ln/mus3/ /ln/torrents/ /ln/mo/blink/torrents_ready/musREPAIR/ /ln/dwn"
 
-find -L  $PATH_OLD  -type f \( -name "*.flac" -or -name "*.mp3" -or -name "*.ogg" -or -name "*.wv" -or -name "*.ape" -or -name "*.wma" \) -printf '%h\n' | sort -u > $muslibrary
-c1=$(wc -l $muslibrary | cut -d " " -f1)
+find -L $(realpath $PATH_OLD) -type f \( -name "*.flac" -or -name "*.mp3" -or -name "*.ogg" -or -name "*.wv" -or -name "*.ape" -or -name "*.wma" \) -printf '%h\n' | sort -u > $muslibrary
+cnt1=$(wc -l $muslibrary | cut -d " " -f1)
 
-find -L  $PATH_NEW  -type f \( -name "*.flac" -or -name "*.mp3" -or -name "*.ogg" -or -name "*.wv" -or -name "*.ape" -or -name "*.wma" \) -printf '%h\n' | sort -u >> $muslibrary
-c2=$(wc -l $muslibrary | cut -d " " -f1)
+find -L $(realpath $PATH_NEW) -type f \( -name "*.flac" -or -name "*.mp3" -or -name "*.ogg" -or -name "*.wv" -or -name "*.ape" -or -name "*.wma" \) -printf '%h\n' | sort -u >> $muslibrary
+cnt2=$(wc -l $muslibrary | cut -d " " -f1)
 
 HASG=false; pgrep -c Xorg &>/dev/null && HASG=true
 
-[ HASG == true ] && noti echo .:. MUSIC FAMILIARITY .:. $c1 of $c2
-echo .:. MUSIC FAMILIARITY .:. $c1 of $c2
+[ HASG==true ] && noti echo .:. MUSIC FAMILIARITY .:. $cnt1 of $cnt2
+echo .:. MUSIC FAMILIARITY .:. $cnt1 of $cnt2
 
