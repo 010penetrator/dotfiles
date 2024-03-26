ccca95bac536c244db6dad309bf616ef59094ed6
plasmik <plasmik@mail.no>
2024-03-26 07:33:31 +0300

    another

--- sh/muslibupd.sh
+++ sh/muslibupd.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 # Save list of music albums in my music folders. Show brief report.
+
 muslibrary=$HOME/.mus-library
 rm $muslibrary
 
-PATH_OLD="/ln/mus"
-PATH_NEW="/ln/torrents /ln/mo/blink/torrents_ready/musREPAIR /ln/dwn /ln/mo/metal/muz__ /ln/mo/metal/muzlo"
+PATH_NEW="/ln/torrents /ln/mo/blink/torrents_ready/musREPAIR /ln/dwn /ln/mus/0*"
+PATH_MOST="/ln/mus/*"
 
-find -L $(realpath $PATH_OLD) -type f \( -name "*.flac" -or -name "*.mp3" -or -name "*.ogg" -or -name "*.wv" -or -name "*.ape" -or -name "*.wma" \) -printf '%h\n' | sort -u > $muslibrary
+find -L $(realpath $PATH_NEW) -type f \( -name "*.flac" -or -name "*.mp3" -or -name "*.ogg" -or -name "*.wv" -or -name "*.ape" -or -name "*.wma" \) -printf '%h\n' | sort -u > $muslibrary
 cnt1=$(wc -l $muslibrary | cut -d " " -f1)
 
-find -L $(realpath $PATH_NEW) -type f \( -name "*.flac" -or -name "*.mp3" -or -name "*.ogg" -or -name "*.wv" -or -name "*.ape" -or -name "*.wma" \) -printf '%h\n' | sort -u >> $muslibrary
+find -L $(realpath $PATH_NEW $PATH_MOST | sort | uniq) -type f \( -name "*.flac" -or -name "*.mp3" -or -name "*.ogg" -or -name "*.wv" -or -name "*.ape" -or -name "*.wma" \) -printf '%h\n' | sort -u > $muslibrary
 cnt2=$(wc -l $muslibrary | cut -d " " -f1)
 
 HASG=false; pgrep -c Xorg &>/dev/null && HASG=true
 
-[ HASG==true ] && noti echo .:. MUSIC FAMILIARITY .:. $cnt1 of $cnt2
-echo .:. MUSIC FAMILIARITY .:. $cnt1 of $cnt2
+[ HASG==true ] && noti echo .:. NEW MUSIC .:. $cnt1 of $cnt2
+echo .:. NEW MUSIC .:. $cnt1 of $cnt2
 
